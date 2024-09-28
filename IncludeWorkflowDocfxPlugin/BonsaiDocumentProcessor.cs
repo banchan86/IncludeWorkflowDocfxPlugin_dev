b38e2a09a7f51d9299d7db966087625992e70e13
Shawn Tan <banchan@gmail.com>
2024-09-27 18:58:17 -0700

    Initial draft based on docfx plugin tutorial

---
+++ IncludeWorkflowDocfxPlugin/BonsaiDocumentProcessor.cs
@@ -0,0 +1,17 @@
+﻿[Export(typeof(IDocumentProcessor))]
+public class BonsaiDocumentProcessor : IDocumentProcessor
+{
+    // todo : implements IDocumentProcessor.
+}
+
+public ProcessingPriority GetProcessingPriority(FileAndType file)
+{
+    if (file.Type == DocumentType.Article &&
+        ".bonsai".Equals(Path.GetExtension(file.File), StringComparison.OrdinalIgnoreCase))
+    {
+        return ProcessingPriority.Normal;
+    }
+    return ProcessingPriority.NotSupported;
+}
+
+
